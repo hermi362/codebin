ee7cc6d5f44f89491f89a23a44694cf945cec7e0
Herminio Gonzalez <herminio@gmail.com>
2022-04-16 08:02:01 +0900

    Update bash-fu.sh

--- bash-fu.sh
+++ bash-fu.sh
@@ -2,3 +2,6 @@
 # Handy for batch renaming stuff, like subtitle files.
 
 for dirname in $(ls Subs); do echo cp -i Subs/$dirname/* ./$dirname.srt; done
+for dirname in Subs/*;     do echo cp -i Subs/$dirname/* ./$dirname.srt; done
+# The second version uses globbing, which is better because if a certain filename has a space in it, the first version will split that filename into two.
+
